41b99209c193114176f844cd401e423442686b85
Vishvesh Sawant <vishveshsavant@gmail.com>
2016-10-13 02:04:31 +0530

    Adding after changes

--- ElementLocators.py
+++ ElementLocators.py
@@ -1,15 +1,14 @@
 __author__ = 'Vishvesh Savant'
 
-from Main_File import driver
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 
-class CreateAccountElements:
+class CreateAccountElements(object):
 
-    def Elements(self):
+    def Elements(self, driver):
         Email_Address= driver.find_element_by_xpath(".//*[@id='email_create']")
         Email_Address.clear()
         Email_Address.send_keys("amdv1991@gmail.com")
 
-    def ClickCreateAccountButton(self):
+    def ClickCreateAccountButton(self, driver):
         driver.find_element_by_xpath(".//*[@id='SubmitCreate']").click()
